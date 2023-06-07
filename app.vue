a3ee5d722b59b91c3bb9395e8d98db38e00b4ff2
ahmedragab <ahmedragab20901@gmail.com>
2023-06-08 01:43:28 +0300

    🙌🏻 Add nuxt-ui

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <div class="flex justify-center items-center h-screen">
+      <div>
+        Play with nitro!
+      </div>
+    </div>
   </div>
 </template>
