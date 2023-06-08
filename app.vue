10f8c52ab41ad2e6f0a299739680b4ec6ade4c9c
ahmedragab <ahmedragab20901@gmail.com>
2023-06-08 20:09:16 +0300

    ..

--- app.vue
+++ app.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <div class="flex justify-center items-center h-screen">
-      <div>
-        Play with nitro!
-      </div>
+      <div class="border border-slate-200 rounded-lg px-2 py-1">Play with nitro!</div>
     </div>
   </div>
 </template>
+<script setup lang="ts">
+  // write a function
+  const playWithNitro = () => {
+    console.log('play with nitro');
+  };
+</script>
